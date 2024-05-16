4f7d6a7a5683138e80ea67c542a4b5c9d26ad7c8
Erick Simões <erick.simoes@live.com>
2024-05-16 17:18:44 -0300

    Add test script to print sensor state

--- portal.ino
+++ portal.ino
@@ -1,8 +1,10 @@
+const int presenceSensorPin = 2;
 
 void setup() {
-
+  Serial.begin(9600);
+  pinMode(presenceSensorPin, INPUT);
 }
 
 void loop() {
-
+  Serial.println(digitalRead(presenceSensorPin));
 }
